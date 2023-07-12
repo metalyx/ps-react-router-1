11c4993d7c5e0f31fc559af2e276eb6c9308840d
Vitalii Tereshchenko <metalyx3@gmail.com>
2023-07-11 17:51:05 -0700

    done

---
+++ src/pages/Price.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function Price(props) {
+  // Chase, token is unavailable at this resource for now
+  const apiKey = "YOUR API KEY";
+
+  const params = useParams();
+  const symbol = params.symbol;
+  // Chase, token is unavailable at this resource for now
+  const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+
+  const [coin, setCoin] = useState("null");
+
+  const getCoin = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCoin(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getCoin();
+  }, []);
+
+  const loaded = () => {
+    return (
+      <div>
+        <h1>
+          {coin.asset_id_base}/{coin.asset_id_quote}
+        </h1>
+        <h2>{coin.rate}</h2>
+      </div>
+    );
+  };
+
+  // Function for when data doesn't exist
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  // if coin has data, run the loaded function, otherwise, run loading
+  return coin && coin.rate ? loaded() : loading();
+}
